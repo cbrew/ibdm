0a4c35fa6490d03a394d1689adc56de4359ee852
Chris Brew <brewc@dy6nm2ng4q.legal.regn.net>
2025-11-21 12:55:55 -0500

    fix(rules): export selection factories

--- src/ibdm/rules/__init__.py
+++ src/ibdm/rules/__init__.py
@@ -7,11 +7,18 @@ IBiS2 Extensions:
 - ICM (Interactive Communication Management) integration rules for grounding
 """
 
-from ibdm.rules.action_rules import create_action_integration_rules
+from ibdm.rules.action_rules import (
+    create_action_integration_rules,
+    create_action_selection_rules,
+)
 from ibdm.rules.generation_rules import create_generation_rules
 from ibdm.rules.icm_integration_rules import create_icm_integration_rules
 from ibdm.rules.integration_rules import create_integration_rules
 from ibdm.rules.interpretation_rules import create_interpretation_rules
+from ibdm.rules.negotiation_rules import (
+    create_negotiation_rules,
+    create_negotiation_selection_rules,
+)
 from ibdm.rules.selection_rules import create_selection_rules
 from ibdm.rules.update_rules import RuleSet, UpdateRule
 
@@ -21,7 +28,10 @@ __all__ = [
     "create_interpretation_rules",
     "create_integration_rules",
     "create_action_integration_rules",
+    "create_action_selection_rules",
     "create_icm_integration_rules",
+    "create_negotiation_rules",
+    "create_negotiation_selection_rules",
     "create_selection_rules",
     "create_generation_rules",
 ]
